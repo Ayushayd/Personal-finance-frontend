e3ad5c722edd26303ae5b4bfd04c3165ad8f8de0
Ayush Gupta <ayushe19222@gmail.com>
2025-07-09 00:22:34 +0530

    changed backend url from railway to render

--- personal-finance/src/api/api.js
+++ personal-finance/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://finance-system1-production.up.railway.app/api",
+  baseURL: "https://finance-system1.onrender.com/api",
 });
 
 // Attach token to all requests if user is logged in
